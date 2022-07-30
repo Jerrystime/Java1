977082ec910aeeca1093ced942abd5d6ebc8719d
Hyeonseong Lee <hslee7231@gmail.com>
2022-07-30 18:19:40 +0900

    클래스

---
+++ Progamming/ClassApp.java
@@ -0,0 +1,13 @@
+
+public class ClassApp {
+
+	public static void main(String[] args) {
+		
+		// 클래스는 서로 연관된 변수와 메소드를 그룹핑하여 이름을 붙인 것이다.
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(1.6));
+		System.out.println(Math.ceil(1.6));
+		
+	}
+
+}
